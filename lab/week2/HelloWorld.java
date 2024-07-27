55d07248a0854ed691fcae68f27b9ae15762c58c
Sirisuk Tharntham <79169700+punsnx@users.noreply.github.com>
2024-07-27 21:16:16 +0700

    update lab upto week 5

---
+++ lab/week2/HelloWorld.java
@@ -0,0 +1,10 @@
+// usign Carmel case dont use _
+public class HelloWorld{
+    // for constant varibale 
+    private static final double MATH_PI = 3.14159;
+    public static void mian(String[] args){
+        System.out.println("Hello, World");
+        // out static attribute 
+        // println is atrribute of out
+    }
+}
